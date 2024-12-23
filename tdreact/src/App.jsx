f46f97aa56c56cd05ab39809dfdf02e7a73be56b
tartalt <127695007+tartalt@users.noreply.github.com>
2024-12-23 17:17:04 +0100

    Add menu

--- tdreact/src/App.jsx
+++ tdreact/src/App.jsx
@@ -43,7 +43,7 @@ function DisplayItem({ item }) {
     return (
         <div>
             <h2>{item.course}</h2>
-            <p>Étudiant : {item.student.firstname} {item.student.lastname}</p>
+            <p>Étudiant : {item.student.firstname} {item.student.lastname} id : {item.student.id}</p>
             <p>Date : {item.date}</p>
             <p>Note : {item.grade}</p>
         </div>
@@ -56,36 +56,52 @@ function getRandomItem(data) {
 function App() {
     const [count, setCount] = useState(0)
     const randomItem = getRandomItem(data);
+    const menuItems = [
+        { id: 1, text: randomItem.student.lastname, alertText: randomItem.student.firstname },
+        { id: 2, text: 'Matières', alertText: randomItem.course},
+        { id: 3, text: 'Note', alertText: randomItem.grade },
+        { id: 4, text: 'Date', alertText: randomItem.date },
+    ];
+    const handleClick = (text) => {
+        alert(text);
+    };
     return (
-        <>
+        <div>
+            <nav>
+                <ul>
+                    {menuItems.map((item) => (
+                        <li key={item.id} onClick={() => handleClick(item.alertText)}>
+                            {item.text}
+                        </li>
+                    ))}
+                </ul>
+            </nav>
             <div>
-                <Header name="React"/>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-
-      </div>
+                <Header name="React" />
+                <a href="https://vite.dev" target="_blank">
+                    <img src={viteLogo} className="logo" alt="Vite logo" />
+                </a>
+                <a href="https://react.dev" target="_blank">
+                    <img src={reactLogo} className="logo react" alt="React logo" />
+                </a>
+            </div>
             <MainContent />
-      <h1>Vite + React</h1>
-      <div className="card">
-
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-          <DisplayItem item={randomItem} />
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-      <Footer />
-    </>
-  )
+            <h1>Vite + React</h1>
+            <div className="card">
+                <button onClick={() => setCount((count) => count + 1)}>
+                    count is {count}
+                </button>
+                <p>
+                    Edit <code>src/App.jsx</code> and save to test HMR
+                </p>
+                <DisplayItem item={randomItem} />
+            </div>
+            <p className="read-the-docs">
+                Click on the Vite and React logos to learn more
+            </p>
+            <Footer />
+        </div>
+    );
 }
 
 export default App
