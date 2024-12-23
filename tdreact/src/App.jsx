43cc9133d1f4f0be2dac6966ca9930cee08915f3
tartalt <127695007+tartalt@users.noreply.github.com>
2024-12-23 16:11:37 +0100

    TD03 : Afficher les détails d'une note

--- tdreact/src/App.jsx
+++ tdreact/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import data from './assets/data.json';
 
 function Header(props) {
     // let name = "React"
@@ -27,6 +28,8 @@ function MainContent() {
         </main>
     )
 }
+
+
 function Footer() {
     const date = new Date();
     const year = date.getFullYear();
@@ -36,10 +39,23 @@ function Footer() {
         </footer>
     )
 }
-
+function DisplayItem({ item }) {
+    return (
+        <div>
+            <h2>{item.course}</h2>
+            <p>Étudiant : {item.student.firstname} {item.student.lastname}</p>
+            <p>Date : {item.date}</p>
+            <p>Note : {item.grade}</p>
+        </div>
+    );
+}
+function getRandomItem(data) {
+    const randomIndex = Math.floor(Math.random() * data.length);
+    return data[randomIndex];
+}
 function App() {
     const [count, setCount] = useState(0)
-
+    const randomItem = getRandomItem(data);
     return (
         <>
             <div>
@@ -62,6 +78,7 @@ function App() {
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
+          <DisplayItem item={randomItem} />
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
