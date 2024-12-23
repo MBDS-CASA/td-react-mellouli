f254634cd7a527f6880939f7197e809c80dc67b0
tartalt <127695007+tartalt@users.noreply.github.com>
2024-12-23 15:43:08 +0100

    TD01 : Ajouter un composant pour le contenu principal de la page

--- tdreact/src/App.jsx
+++ tdreact/src/App.jsx
@@ -3,14 +3,21 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function Header() {
-    let name = "React"
+function Header(props) {
+    // let name = "React"
     return (
         <header>
             <img src="https://emsi.ma/wp-content/uploads/2020/07/logo.png"  title="logo" alt="logoemsi"/>
-            <h1>Introduction à {name}</h1>
+            <h1>Introduction à {props.name}</h1>
             <h2>A la découverte des premières notions de React</h2>
         </header>
+    )
+}
+function MainContent() {
+    return (
+        <main>
+            <p>Ici, nous afficherons des informations interessantes :)</p>
+        </main>
     )
 }
 
@@ -20,16 +27,18 @@ function App() {
     return (
         <>
             <div>
-                <Header />
+                <Header name="React"/>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
+                <MainContent />
       </div>
       <h1>Vite + React</h1>
       <div className="card">
+
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
